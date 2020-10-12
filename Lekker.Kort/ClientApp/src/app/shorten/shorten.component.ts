89a0ff7d986857a149218fe604177864d662037c
Ettienne Scharneck <EttienneS@k2.com>
2020-10-12 21:23:54 +0200

    Cleanup typescript for testing Add initial test

--- Lekker.Kort/ClientApp/src/app/shorten/shorten.component.ts
+++ Lekker.Kort/ClientApp/src/app/shorten/shorten.component.ts
@@ -1,23 +1,18 @@
 import { Component, Inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { MatIconRegistry } from '@angular/material/icon';
-import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-shorten',
   templateUrl: './shorten.component.html',
   styleUrls: ['./shorten.component.css']
 })
+
 export class ShortenComponent {
-  constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string, iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
+  constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
     this.shortenEndpoint = baseUrl + "ShortUrl";
     this.lekkerEndpoint = baseUrl + "LekkerUrl";
     this.redirectionEndpoint = baseUrl + "r/";
     this.http = http;
-
-    iconRegistry.addSvgIcon(
-      'copy',
-      sanitizer.bypassSecurityTrustResourceUrl('assets/content_copy-24px.svg'));
   }
 
   private redirectionEndpoint: string;
@@ -26,23 +21,23 @@ export class ShortenComponent {
   private http: HttpClient;
 
   public modifiedUrl: string;
-  public showModified: boolean;
-
-  public getShortUrl(url: string) {
-    this.http.post<ShortUrl>(this.shortenEndpoint, { url: url }).subscribe(result => {
-      this.modifiedUrl = this.redirectionEndpoint + result.shortUrl;
-      this.showModified = true;
-    }, error => console.error(error));
-  }
 
   public getLekkerUrl(url: string) {
-    this.http.post<ShortUrl>(this.lekkerEndpoint, { url: url }).subscribe(result => {
+    this.getModifiedUrlFromEndpoint(url, this.lekkerEndpoint);
+  }
+
+  public getShortUrl(url: string) {
+    this.getModifiedUrlFromEndpoint(url, this.shortenEndpoint);
+  }
+
+  public getModifiedUrlFromEndpoint(url: string, endpoint: string) {
+    this.modifiedUrl = '';
+    this.http.post<ModifiedUrl>(endpoint, { url: url }).subscribe(result => {
       this.modifiedUrl = this.redirectionEndpoint + result.shortUrl;
-      this.showModified = true;
     }, error => console.error(error));
   }
 }
 
-interface ShortUrl {
+interface ModifiedUrl {
   shortUrl: string;
 }
